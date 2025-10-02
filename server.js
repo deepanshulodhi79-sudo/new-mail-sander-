180dd4a38200091da8e9615f74375b6d9b9a8724
deepanshulodhi79-sudo <deepanshulodhi79@gmail.com>
2025-10-02 23:01:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // 🔑 Hardcoded login
-const HARD_USERNAME = "Yatendra Rajput";
-const HARD_PASSWORD = "Yattu@882";
+const HARD_USERNAME = "Radha Krishna";
+const HARD_PASSWORD = "shree krishna15";
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
